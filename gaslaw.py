17ba714eb54b4a038be796b779a397af4fb8b071
nimansh761 <flashyy761@gmail.com>
2025-04-24 18:11:37 -0600

    Update gaslaw.py

--- gaslaw.py
+++ gaslaw.py
@@ -2,7 +2,7 @@ from manim import *
 
 class GasLawDerivation(Scene):
     def construct(self):
-        # Initial equation
+        
         original = MathTex("P", "V", "=", "n", "R", "T")
         original.scale(1.5)
         colors = [BLUE, RED, WHITE, GREEN, GOLD, PURPLE]
@@ -12,13 +12,13 @@ class GasLawDerivation(Scene):
         self.play(Write(original))
         self.wait(2)
 
-        # Step 1: Substitute n = m/M
+        
         sub1 = MathTex("n", "=", r"\frac{m}{M}").next_to(original, DOWN)
         sub1[0].set_color(GREEN)
         self.play(Write(sub1))
         self.wait(1)
 
-        # Transform to PV = (m/M)RT
+        
         eq1 = MathTex("P", "V", "=", r"\frac{m}{M}", "R", "T")
         eq1.scale(1.5)
         for i, color in [(0, BLUE), (1, RED), (4, GOLD), (5, PURPLE)]:
@@ -31,7 +31,7 @@ class GasLawDerivation(Scene):
         )
         self.wait(1)
 
-        # Step 2: Divide both sides by V
+        
         eq2 = MathTex("P", "=", r"\frac{m}{M \cdot V}", "R", "T")
         eq2.scale(1.5)
         for i, color in [(0, BLUE), (3, GOLD), (4, PURPLE)]:
@@ -40,13 +40,13 @@ class GasLawDerivation(Scene):
         self.play(Transform(original, eq2), run_time=2)
         self.wait(1)
 
-        # Step 3: Substitute D = m/V
+        
         sub2 = MathTex(r"\rho", "=", r"\frac{m}{V}").next_to(original, DOWN)
         sub2[0].set_color(ORANGE)
         self.play(Write(sub2))
         
 
-        # Final equation
+        
         final = MathTex("P", "=", r"\frac{\rho}{M}", "R", "T")
         final.scale(1.5)
         for i, color in [(0, BLUE), (2, ORANGE), (3, GOLD), (4, PURPLE)]:
